3721cbb428b10810dc6e51158934a596fd86a9ef
amir laptop <amirhosseinranjbar.net@gmail.com>
2025-04-26 22:51:18 +0330

    dashboard layout corrected

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -8,11 +8,20 @@ export default function DashboardLayout({ children }) {
       <DashboardHeader />
       <section className="container mx-auto p-3">
 
-        <div className="flex items-center bg-amber-300 mx-auto gap-x-3">
-          <div className="w-[30%] bg-sky-300 h-full rounded-lg">
+        <div className="grid lg:grid-cols-12 mx-auto gap-5">
+          <div className="lg:col-span-4 bg-sky-300 h-full rounded-lg">
             money card
           </div>
-          <div className="w-[70%] h-full overflow-hidden">
+          <div className="lg:col-span-8 h-full overflow-hidden">
+            <DashboardBanner />
+          </div>
+        </div>
+
+        <div className="grid lg:grid-cols-12 mx-auto gap-2 mt-3">
+          <div className="lg:col-span-3 bg-sky-300 h-full rounded-lg">
+            money card
+          </div>
+          <div className="lg:col-span-9 h-full overflow-hidden">
             <DashboardBanner />
           </div>
         </div>
